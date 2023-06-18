e3491ab8c8049ba9727889cdaec270c843b2fee0
Aerowiel <pro.florian.pendaries@gmail.com>
2023-06-18 22:03:51 +0200

    _index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,53 +1,38 @@
-export function headers({
-  loaderHeaders,
-  parentHeaders,
-}: {
-  loaderHeaders: Headers;
-  parentHeaders: Headers;
-}) {
-  console.log(
-    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
-  );
-  return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
-    "Cache-Control": "public, max-age=60, s-maxage=60",
-  };
-}
+import { ActionArgs, V2_MetaFunction, redirect } from "@remix-run/node";
+import { json } from "react-router";
+import { v4 as uuidv4 } from "uuid";
+import { createUserSession } from "~/session.server";
+import JoinChat from "~/theme/pages/JoinChat";
 
-export default function Index() {
-  return (
-    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer noopener"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer noopener"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer noopener"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </main>
-  );
-}
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Zanon" },
+    { name: "description", content: "Zanon, anonymous chat" },
+  ];
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const action = formData.get("_action") as string;
+  const pseudo = formData.get("pseudo") as string;
+
+  switch (action) {
+    case "create-new-chat":
+      const randomChatId = uuidv4();
+      return createUserSession({ request, chatId: randomChatId, pseudo });
+    case "join-existing-chat":
+      const chatId = formData.get("chat-id") as string;
+      if (!chatId) {
+        return redirect("/");
+      }
+      return createUserSession({ request, chatId, pseudo });
+    default:
+      return json("unrecognized action: ", { status: 400 });
+  }
+};
+
+const IndexRoute = () => {
+  return <JoinChat />;
+};
+
+export default IndexRoute;
